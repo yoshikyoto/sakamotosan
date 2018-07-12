db6ab5c71a8b9e08e61eac1892cee7349f71da83
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-07-12 15:47:08 +0900

    モジュールの追加をしやすくして見やすくして

--- bot.js
+++ bot.js
@@ -1,37 +1,28 @@
-var Botkit = require('botkit');
+let Botkit = require('botkit');
 let middleware = require('./lib/botkit/middleware.js');
 
-var pomodoro = require('./lib/botkit/middlewares/pomodoro.js');
+let pomodoro = require('./lib/botkit/middlewares/pomodoro.js');
+let xayahrakan = require('./lib/botkit/middlewares/xayahrakan.js');
 
 // .envを環境変数の値としてセット
 require('dotenv').config()
 
-var controller = Botkit.slackbot({
+let controller = Botkit.slackbot({
   debug: true,
 });
 
 // Real Time Messaging API 開始
-var bot = controller.spawn({
+let bot = controller.spawn({
   token: process.env.SLACK_TOKEN
 }).startRTM();
 
 // 設定するミドルウェア（対話モジュール）
 let middlewares = [
-  pomodoro
+  pomodoro,
+  xayahrakan,
 ];
 
 // ミドルウェアを順番に設定
 for (items of middlewares) {
   middleware.append(controller, items.middleware);
 }
-
-controller.hears(
-  ['ザヤラカン'],
-  'direct_mention',
-  function(bot, message) {
-    bot.reply(
-      message,
-      'https://pbs.twimg.com/media/DUFk6nVVMAEo2ZN.jpg'
-    );
-  }
-);
