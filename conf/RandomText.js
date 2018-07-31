8e7b59be07f61eb91b8ffae65582892937a02bd1
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-07-31 10:02:19 +0900

    config分離

---
+++ conf/RandomText.js
@@ -0,0 +1,10 @@
+module.exports = [
+  'オリンピックのチケットそろそろ発売してるかもな https://tokyo2020.org/jp/games/ticket/',
+  'https://github.com/ に草は生やしたか？',
+  'ちゃんとブログ更新してるか？',
+  '呼び出し手当ちゃんとつけてるか？',
+  'レビュー見てるか？',
+  'https://github.com/kskgroup',
+  'くこルくん',
+  '無料ニコニ広告ガチャしたか？ https://nicoad.nicovideo.jp/',
+];
