a83aac04a68c7fd191172471cbf45e13a2cbe260
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-08-16 14:08:57 +0900

    add random text

--- conf/RandomText.js
+++ conf/RandomText.js
@@ -8,4 +8,6 @@ module.exports = [
   'くこルくん',
   '無料ニコニ広告ガチャしたか？ https://nicoad.nicovideo.jp/',
   'dreddはOpanAPI 3.0に対応したかなー https://github.com/apiaryio/dredd/issues/894',
+  '今隣駅にいるので少し遅れます。',
+  'ヤクザに絡まれたので遅れます。',
 ];
