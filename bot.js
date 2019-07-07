0a1e6ea38c368dfb894d7c1a6f5a43e3d476b557
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-07-07 16:48:51 +0900

    simplize

--- bot.js
+++ bot.js
@@ -1,14 +1,11 @@
 let Botkit = require('botkit');
-let Pomodoro = require('./lib/botkit/middlewares/pomodoro.js');
-let XayahRakan = require('./lib/botkit/middlewares/xayahrakan.js');
-let Working = require('./lib/botkit/middlewares/Working.js');
-let Task = require('./lib/botkit/middlewares/Task.js');
+let TrelloClient = require('./external/trello/TrelloClient.js');
 
 // .envを環境変数の値としてセット
 require('dotenv').config()
 
 let controller = Botkit.slackbot({
-  debug: true,
+  debug: process.env.DEBUG,
 });
 
 // Real Time Messaging API 開始
@@ -16,15 +13,57 @@ let bot = controller.spawn({
   token: process.env.SLACK_TOKEN
 }).startRTM();
 
-// 設定するミドルウェア（対話モジュール）
-let middlewares = [
-  new Task(),
-  new Pomodoro(),
-  new XayahRakan(),
-  new Working(),
-];
+controller.hears(
+  ['タスク'],
+  'ambient',
+  function(bot, message) {
+    console.log(message.channel);
+    let trello = new TrelloClient(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
+    // 指定した名前のリストのIDを取得し、
+    // そのIDからリストに含まれるチケットを全て取得する
+    trello.getList(
+      process.env.TRELLO_BOARD_ID,
+      process.env.TRELLO_LIST_NAME
+    ).then((list) => {
+      trello.getActiveCardsOnList(
+        process.env.TRELLO_BOARD_ID,
+        list.id
+      ).then((cards) => {
+        let cardNames = cards.map(card => card.name);
+        let text = cardNames.map(name => '- ' + name).join("\n");
+        bot.reply(message, text);
+      }).catch((error) => {
+        bot.reply(message, 'なんかあかんかったわ。');
+      });
+    }).catch((error) => {
+      bot.reply(message, 'なんかあかんかったわ。ボードIDかリスト名がおかしいんちゃう。');
+    });
+  }
+);
 
-// ミドルウェアを順番に設定
-for (middleware of middlewares) {
-  middleware.install(controller);
+
+let timerComment = function() {
+  let trello = new TrelloClient(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
+
+  // 指定した名前のリストのIDを取得し、
+  // そのIDからリストに含まれるチケットを全て取得する
+  trello.getList(
+    process.env.TRELLO_BOARD_ID,
+    process.env.TRELLO_LIST_NAME
+  ).then((list) => {
+    trello.getActiveCardsOnList(
+      process.env.TRELLO_BOARD_ID,
+      list.id
+    ).then((cards) => {
+      let cardNames = cards.map(card => card.name);
+      let text = cardNames.map(name => '- ' + name).join("\n");
+      bot.say({
+        text: text,
+        channel: process.env.SLACK_CHANNEL_ID,
+      });
+    });
+  });
 }
+
+setInterval(timerComment, 30 * 60 * 1000)
+timerComment();
