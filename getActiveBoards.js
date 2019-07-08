7244f3e828c66092f3c921c3f024806a01a8ea13
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-07-08 11:56:02 +0900

    updateGetActiveBoard

--- getActiveBoards.js
+++ getActiveBoards.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
-let key = process.env.TRELLO_KEY;
-let token = process.env.TRELLO_TOKEN;
 
-let TrelloClient = require('./lib/trello/TrelloClient.js');
-let trello = new TrelloClient(key, token);
+let TrelloClient = require('./external/trello/TrelloClient.js');
+let trello = new TrelloClient(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 trello.getActiveBoards().then((boards) => {
   for (let board of boards) {
     console.log(board.id + ' ' + board.name);
