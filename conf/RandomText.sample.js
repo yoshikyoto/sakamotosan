b1ffb6e2548eb59a9cd611cb97868644a5226484
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-07-08 16:15:43 +0900

    randomtect config sample

--- conf/RandomText.sample.js
+++ conf/RandomText.sample.js
@@ -1,9 +1,7 @@
 module.exports = [
   'https://github.com/ に草は生やしたか？',
   'ちゃんとブログ更新してるか？',
-  '呼び出し手当ちゃんとつけてるか？',
   'レビュー見てるか？',
   "無料ニコニ広告ガチャしたか？\nhttps://nicoad.nicovideo.jp/",
   "IPO試験の申し込み忘れていないか？\nhttps://www.ipokiso.com/company/index.html",
-  'https://files.slack.com/files-pri/T027WHQMQ-FL6J86A8L/image.png',
 ];
